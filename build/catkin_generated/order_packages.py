8ac83483641cabd2feb0e17f36918062f2f54ccf
Ben Holleran <ben.holleran@airtonomy.ai>
2022-09-28 17:53:40 +0300

    Cleanup

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/root/ros-learning/simple_pub_sub/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/root/ros-learning/simple_pub_sub/devel;/opt/ros/noetic'.split(';') if '/root/ros-learning/simple_pub_sub/devel;/opt/ros/noetic' != '' else []
